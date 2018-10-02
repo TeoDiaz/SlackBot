932df850018949a9d896614da6901cbdbffd69ba
TeoDiaz <tdiazsan@gmail.com>
2018-10-02 22:05:13 +0200

    Slack bot send messages

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-env(__dirname + '/.env')
+const { RTMClient, WebClient } = require("@slack/client");
+const env = require("node-env-file");
+const schedule = require("node-schedule");
+const token = "MYTOKEN";
+const web = new WebClient(token);
+const rtm = new RTMClient(token);
 
-const {RTMClient, WebClient} = require('@slack/client')
-const env = require('node-env-file')
-const schedule = require('node-schedule')
-const token = process.env.TOKEN
-const web = new WebClient(token)
-const rtm = new RTMClient(token)
+env(__dirname + "/.env");
+
+let channelID = "";
+
+rtm.start();
+
+web.channels.list().then(res => {
+  let channel;
+  res.channels.forEach(e => {
+    if (e.is_member) channelID = e.id;
+  });
+
+  rtm.sendMessage("Who wants to have lunch in the best place of the world? ", channelID);
+  
+});

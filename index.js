3a0ba4245e5dcc3f9bccb47b73184263c8fd0c39
TeoDiaz <tdiazsan@gmail.com>
2018-10-03 08:20:14 +0200

    bot recieve messages

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const token =  process.env.SLACK_TOKEN;
 const web = new WebClient(token);
 const rtm = new RTMClient(token);
 
-console.log(token)
 
 let channelID = "";
 
@@ -18,6 +17,14 @@ web.channels.list().then(res => {
     if (e.is_member) channelID = e.id;
   });
 
-  rtm.sendMessage("Who wants to have lunch in the best place of the world? ", channelID);
-  
+  rtm.sendMessage("Who wants to have lunch in the best place of the world?", channelID);
+  rtm.on('message', (event) => {
+    let msg = event.text;
+    console.log(msg)
+    if(msg == 'chicote start') {
+      rtm.sendMessage('Ey! Who is going to have lunch out today?', channelID)
+    } else if(msg == 'chicote stop') {
+      rtm.sendMessage('I Dont want to leave', channelID)
+    }
+  })
 });

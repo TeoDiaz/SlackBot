153c92217a6998b4ba4e35fdafb0039836045160
TeoDiaz <tdiazsan@gmail.com>
2018-10-05 08:42:16 +0200

    create num of groups & users in groups

--- controllers/actions.js
+++ controllers/actions.js
@@ -5,7 +5,7 @@ class Actions {
     this.arrUsers = [];
     this.usersInArr = 0;
   }
-  
+
   readMessage(e, rtm, channel) {
     const message = e.text;
 
@@ -13,28 +13,36 @@ class Actions {
     this.channel = channel;
 
     if (message.includes("yes")) {
-      this.addUsers(e)
+      this.addUsers(e);
     } else if (message.includes("no more")) {
       this.rtm.sendMessage("Goodbye Folks!", this.channel);
     }
   }
 
-  shuffleArray(){
-    this.arrUsers.sort( () => Math.random() - 0.5 )
+  getCeilNum(num) {
+    return Math.ceil(this.usersInArr / num);
+  }
+
+  shuffleArray() {
+    this.arrUsers.sort(() => Math.random() - 0.5);
   }
 
   addUsers(e) {
-      this.arrUsers.push(e.user);
-      this.rtm.sendMessage(" Yeah <@"+e.user+'> is in! :the_horns:', this.channel)
-      .then( res => {
-        this.usersGroups(7)
+    this.arrUsers.push(e.user);
+    this.rtm
+      .sendMessage("Yeah <@" + e.user + "> is in! :the_horns:", this.channel)
+      .then(res => {
+        this.usersGroups(7);
       })
-      .catch( err => console.log('status', err))
+      .catch(err => console.log("status", err));
   }
-  
-  usersGroups(maxNumber){
-    this.usersInArr = this.arrUsers.length
-    this.shuffleArray()
+
+  usersGroups(maxNumber) {
+    this.usersInArr = this.arrUsers.length;
+    this.shuffleArray();
+    const numGroups = this.getCeilNum(maxNumber);
+    const usersPerGroup = this.getCeilNum(numGroups);
+    const smallGroup = (usersInGroup * numGroups) - this.usersInArr;
   }
 }
 
